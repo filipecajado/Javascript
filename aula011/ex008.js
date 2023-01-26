b07b11eeb527711a0355613892a143323bde4459
filipecajado <filipecajado@ucl.br>
2023-01-26 15:10:58 -0300

    Commit Javascript Curso

---
+++ aula011/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+
+
+console.log(`A velocidade do seu carro é ${vel}Km/hr`)
+if (vel > 60){
+  console.log('Você ultrapassou a velocidade permitida. Multado')
+}
+console.log('Dirija sempre usando um cinto de segurança')
---
+++ aula011/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+  console.log('Voce é brasileiro')
+}
+else{
+  console.log('voce é estrangeiro!')
+}
